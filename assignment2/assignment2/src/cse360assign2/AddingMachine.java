bfa0c2abf39f6cce2d4549e8eda9c695bc0509f0
asd28 <asd28@DESKTOP-RNA7MNS>
2019-10-09 19:05:17 -0700

    first update

---
+++ assignment2/assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,36 @@
+//cse360 assignment 2
+//pengfei Lyu
+//1206675492
+//first time Commit to github
+
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
